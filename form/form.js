53c73c9c5a2b42d58036794691562e12e015e2fc
samirvithlani <samir.vithlani83955@gmail.com>
2024-06-11 16:49:30 +0530

    video

---
+++ form/form.js
@@ -0,0 +1,8 @@
+const getData=(e)=>{
+    e.preventDefault();
+    const gender = document.getElementsByName("gender")[0].value;
+    const gender1 = document.getElementsByName("gender")[1].value;
+    console.log(gender)
+    console.log(gender1)
+
+}
